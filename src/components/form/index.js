8e018a684a56e278349d2bf4ef5b64577588b59c
alextjd <alextjd1@gmail.com>
2018-07-24 01:51:07 +0200

    Placeholder for the form.

--- src/components/form/index.js
+++ src/components/form/index.js
@@ -3,8 +3,8 @@ import './styles.css';
 
 const Form = props => {
     return (
-        <form onSubmit={props.getRecipe} autoComplete="off">
-            <input type="text" name="recipeName" className="form-input"/>
+        <form onSubmit={props.getRecipe} autoComplete="off" className="search-form">
+            <input type="text" name="recipeName" className="form-input" placeholder="Type the recipe"/>
             <button className="form-button">Search</button>
         </form>
     );
