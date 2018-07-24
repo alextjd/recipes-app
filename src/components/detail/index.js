3ee39ea1004c40ec3d75d5bf81da9e07ef442529
alextjd <alextjd1@gmail.com>
2018-07-24 13:13:24 +0200

    Created the basic functionality of the detail component.

--- src/components/detail/index.js
+++ src/components/detail/index.js
@@ -1,10 +1,21 @@
 import React, {Component} from 'react';
 
+import './styles.css';
+
 class Detail extends Component {
+    state = {
+        activeRecipe: this.props.location.state,
+    };
+
     render() {
         return (
-            <div>
-                This is the detail
+            <div className="detail-container">
+                <div className="detail-img">
+                    <img src={this.state.activeRecipe.image_url} alt="Recipe image"/>
+                </div>
+                <div className="detail-title">{this.state.activeRecipe.title}</div>
+                <div className="detail-author">{this.state.activeRecipe.publisher}</div>
+                <div className="detail-website">{this.state.activeRecipe.publisher_url}</div>
             </div>
         );
     }
