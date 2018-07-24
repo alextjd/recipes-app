ba878cac320034e053268b6f34196b1dc1cca043
alextjd <alextjd1@gmail.com>
2018-07-24 02:19:37 +0200

    Added the loader to the conditional rendering.

--- src/components/recipes/index.js
+++ src/components/recipes/index.js
@@ -4,25 +4,36 @@ import './styles.css';
 const Recipes = props => {
     let recipes = null;
     if (props.recipes !== null) {
-        recipes = props.recipes.map((item, index) => {
-            return (
-                <div className="col-md-4 col-lg-4" key={item.recipe_id}>
-                    <div className="recipe-item">
-                        <img src={item.image_url} alt="Some food" className="recipe-img" />
-                        <div className="recipe-text">
-                            <div className="recipe-title">
-                                {item.title}
+        //Look for searches with no result
+        if (props.recipes.length === 0) {
+            recipes = <div className="noresult">No results were found for that search :(</div>;
+        }
+        else {
+            recipes = props.recipes.map((item, index) => {
+                return (
+                    <div className="col-md-4 col-lg-4" key={item.recipe_id}>
+                        <div className="recipe-item">
+                            <img src={item.image_url} alt="Some food" className="recipe-img"/>
+                            <div className="recipe-text">
+                                <div className="recipe-title">
+                                    {item.title}
+                                </div>
+                                <div className="recipe-author">
+                                    Created by: <span>{item.publisher}</span>
+                                </div>
+                                <button className="detail-btn">View recipe</button>
                             </div>
-                            <div className="recipe-author">
-                                Created by: <span>{item.publisher}</span>
-                            </div>
-                            <button className="detail-btn">View recipe</button>
                         </div>
                     </div>
-                </div>
-            );
-        });
+                );
+            });
+        }
     }
+
+    else if (props.searching === true) {
+        recipes = <div className="search-loader"></div>
+    }
+
     return (
         <div className="container">
             <div className="row">{recipes}</div>
