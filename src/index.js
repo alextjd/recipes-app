0675887b3edf1a0e05cd5c624edf3dc45f43acd4
alextjd <alextjd1@gmail.com>
2018-07-24 18:02:41 +0200

    Github pages deploy.

--- src/index.js
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import Router from './components/router/index';
-
-ReactDOM.render(<Router />, document.getElementById('root'));
